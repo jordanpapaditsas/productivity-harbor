4d3d02b443da97d9b7204f4e5c8d70a60a29b619
Iordanis Papaditsas <114758586+jordanpapaditsas@users.noreply.github.com>
2025-03-30 21:03:07 +0300

    Added login, register dtos

---
+++ client/src/app/dtos/login.dto.ts
@@ -0,0 +1,4 @@
+export class LoginDto {
+  UserName: string = '';
+  Password: string = '';
+}
---
+++ client/src/app/dtos/register.dto.ts
@@ -0,0 +1,6 @@
+export class RegisterDto {
+  UserName: string = '';
+  Password: string = '';
+  Email: string = '';
+  FullName?: string = '';
+}
