291a56dfabc997a11a7461cb1872864b776ff429
Iordanis Papaditsas <114758586+jordanpapaditsas@users.noreply.github.com>
2025-03-30 20:44:59 +0300

    Added nuget packages

--- api/Program.cs
+++ api/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using ProductivityHarborApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
