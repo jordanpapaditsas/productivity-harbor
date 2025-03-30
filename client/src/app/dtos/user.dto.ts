8bea8467e32babd465b4af86078bec9720430e17
Iordanis Papaditsas <114758586+jordanpapaditsas@users.noreply.github.com>
2025-03-30 20:51:43 +0300

    Added properties for user

--- client/src/app/dtos/user.dto.ts
+++ client/src/app/dtos/user.dto.ts
@@ -2,11 +2,16 @@ import { Guid } from 'guid-typescript';
 
 export class UserDto {
   Id: Guid = Guid.createEmpty();
-  FullName: string = '';
-  Avatar: string = '';
+  UserName?: string = '';
+  PasswordHash?: string = '';
+  Email?: string = '';
+  FullName?: string = '';
+  Avatar?: string = '';
+  Token?: string = '';
+  IsActive?: boolean;
+  IsDeleted?: boolean;
   CreatedAt: Date | string | number = new Date();
-  CreatedBy: Guid = Guid.createEmpty();
-  UpdatedAt: Date | string | number = new Date();
-  UpdatedBy: Guid = Guid.createEmpty();
-  Token: string = '';
+  CreatedById?: Guid = Guid.createEmpty();
+  UpdatedAt?: Date | string | number = new Date();
+  UpdatedById?: Guid = Guid.createEmpty();
 }
