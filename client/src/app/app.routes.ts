8369216eeee179cf0801695fb32d30b1475bc3f6
Iordanis Papaditsas <114758586+jordanpapaditsas@users.noreply.github.com>
2025-04-01 20:27:57 +0300

    Cleanup Code

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,14 +1,3 @@
 import { Routes } from '@angular/router';
-import { RegisterComponent } from './pages/account/components/register/register.component';
-import { LoginComponent } from './pages/account/components/login/login.component';
 
-export const routes: Routes = [
-  {
-    path: 'register',
-    component: RegisterComponent,
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-];
+export const routes: Routes = [];
