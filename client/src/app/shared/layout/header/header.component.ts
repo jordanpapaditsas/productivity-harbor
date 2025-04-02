4b37e11a3b8d06bedb7592ddfd31a031cf05336b
Iordanis Papaditsas <114758586+jordanpapaditsas@users.noreply.github.com>
2025-04-02 23:51:20 +0300

    Adding Material Components

--- client/src/app/shared/layout/header/header.component.ts
+++ client/src/app/shared/layout/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-  standalone: true,
+  imports: [MatToolbarModule],
 })
 export class HeaderComponent implements OnInit {
   constructor() {}
--- client/src/app/shared/layout/side-nav/side-nav.component.ts
+++ client/src/app/shared/layout/side-nav/side-nav.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.css']
+  styleUrls: ['./side-nav.component.css'],
+  imports: [MatSidenavModule],
 })
 export class SideNavComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
