fde41e8ca854af3044bfe5fdd71a0b553cec34aa
Iordanis Papaditsas <114758586+jordanpapaditsas@users.noreply.github.com>
2025-04-13 23:30:03 +0300

    Added withViewTransitions() method for routing

--- client/src/app/config/app.config.ts
+++ client/src/app/config/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from '../app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
@@ -11,7 +11,7 @@ import {
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withViewTransitions()),
     provideHttpClient(),
     provideAnimations(),
     provideNativeDateAdapter(),
