9a005fb7dd8c3c2313d392ffad935a1a102ee96b
Iordanis Papaditsas <114758586+jordanpapaditsas@users.noreply.github.com>
2025-04-13 21:08:39 +0300

    Migration for social media model

---
+++ api/Migrations/20250413175435_AddedSocialMediaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProductivityHarborApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSocialMediaController : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
