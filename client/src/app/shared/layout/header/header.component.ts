5f2e5c10c293028c4a942d21bf4006cdac97971b
Iordanis Papaditsas <114758586+jordanpapaditsas@users.noreply.github.com>
2025-04-21 23:06:56 +0300

    Added animation in theme switching button

--- client/src/app/shared/layout/header/header.component.ts
+++ client/src/app/shared/layout/header/header.component.ts
@@ -2,12 +2,21 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { ThemeService } from '../../services/theme.service';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
   imports: [MatToolbarModule, MatIconModule],
+  animations: [
+    trigger('switchTheme', [
+      transition(':enter', [
+        style({ opacity: 0, height: '0px' }),
+        animate('600ms ease-in-out', style({ opacity: 1, height: '*' })),
+      ]),
+    ]),
+  ],
 })
 export class HeaderComponent implements OnInit {
   theme = signal<string>('');
