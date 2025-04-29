00d612b5e5f578f6ae124a74f62a7dc8425d0aed
Iordanis Papaditsas <114758586+jordanpapaditsas@users.noreply.github.com>
2025-04-29 22:20:42 +0300

    Created Login Form with reactive forms of angular

--- client/src/app/features/auth/login/login.component.ts
+++ client/src/app/features/auth/login/login.component.ts
@@ -1,11 +1,16 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { PhTextBoxComponent } from '../../../shared/components/ph-text-box/ph-text-box.component';
 import { AuthService } from '../../../shared/services/auth.service';
 import { LoginDto } from '../../../core/dto/auth/login.dto';
 import { Router } from '@angular/router';
 import { PhLoadingSpinnerComponent } from '../../../shared/components/ph-loading-spinner/ph-loading-spinner.component';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -14,40 +19,55 @@ import { PhLoadingSpinnerComponent } from '../../../shared/components/ph-loading
   imports: [
     MatFormFieldModule,
     MatIconModule,
-    PhTextBoxComponent,
     PhLoadingSpinnerComponent,
+    ReactiveFormsModule,
   ],
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
   protected loginDto: LoginDto;
   protected isLoading = signal<boolean>(false);
   private authService = inject(AuthService);
   private router = inject(Router);
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
     this.loginDto = new LoginDto();
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
 
   login() {
-    this.isLoading.set(true);
-    this.authService.login(this.loginDto).subscribe({
-      next: (response) => {
-        if (response.IsSuccess) {
+    debugger;
+    if (this.loginForm.valid) {
+      this.loginDto.UserName = this.loginForm.value.username;
+      this.loginDto.Password = this.loginForm.value.password;
+      this.isLoading.set(true);
+      this.authService.login(this.loginDto).subscribe({
+        next: (response) => {
+          if (response.IsSuccess) {
+            this.isLoading.set(false);
+            console.log(response.Message);
+            this.router.navigate(['/admin-home']);
+          } else {
+            console.log(response.Error);
+            this.isLoading.set(false);
+          }
+        },
+        error: (error) => {
+          console.log('Server Error', error);
           this.isLoading.set(false);
-          console.log(response.Message);
-          this.router.navigate(['/admin-home']);
-        } else {
-          console.log(response.Error);
-          this.isLoading.set(false);
-        }
-      },
-      error: (error) => {
-        console.log('Server Error', error);
-        this.isLoading.set(false);
-      },
-    });
+        },
+      });
+    }
   }
 
   logout() {
