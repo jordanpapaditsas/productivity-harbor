6fce19eca277203a2d90a1a0e0d2600189b3729c
Iordanis Papaditsas <i.papaditsas@smarttree.gr>
2026-06-02 23:31:34 +0300

    Implemented module DTO

--- client/src/app/core/dtos/shared/module.dto.ts
+++ client/src/app/core/dtos/shared/module.dto.ts
@@ -2,9 +2,12 @@ import { Guid } from 'guid-typescript';
 
 export class ModuleDto {
   Id!: Guid;
+  ParentId?: Guid;
   Key: string = '';
   Label: string = '';
   Icon: string = '';
   Route: string = '';
+  SortOrder?: number;
+  IsVisible: boolean = false;
   SubModules?: ModuleDto[];
 }
